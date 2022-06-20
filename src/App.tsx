5405fff95b7fee4ab36e6d28a113efc8359abede
yerin jeon <rin9682@daum.net>
2022-06-20 09:32:27 +0900

    Add hompage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React, { useEffect, useRef } from 'react';
-// import '@emotion/react';
 import styled from '@emotion/styled';
 import './App.css';
 import * as tf from '@tensorflow/tfjs';
 import * as tmImage from '@teachablemachine/image';
-import { AnyTxtRecord } from 'dns';
 
 const Container = styled.div`
   font-size: 1.2rem;
-  /* display: flex; */
-  /* flex-direction: column; */
 `;
 
 const Title = styled.div`
@@ -43,28 +39,22 @@ function App() {
     let maxPredictions: number;
     let webcam: tmImage.Webcam;
 
-    // Load the image model and setup the webcam
     async function init() {
         const modelURL = URL + "model.json";
         const metadataURL = URL + "metadata.json";
 
-        // load the model and metadata
-        // Refer to tmImage.loadFromFiles() in the API to support files from a file picker
-        // or files from your local hard drive
-        // Note: the pose library adds "tmImage" object to your window (window.tmImage)
+        
         model = await tmImage.load(modelURL, metadataURL);
         maxPredictions = model.getTotalClasses();
 
-        // Convenience function to setup a webcam
-        const flip = true; // whether to flip the webcam
-        webcam = new tmImage.Webcam(400, 400, flip); // width, height, flip
-        await webcam.setup(); // request access to the webcam
+        const flip = true; 
+        webcam = new tmImage.Webcam(400, 400, flip); 
+        await webcam.setup(); 
         await webcam.play();
         window.requestAnimationFrame(loop);
 
-        // append elements to the DOM
         webcamContainerEl.current?.appendChild(webcam.canvas);
-        for (let i = 0; i < maxPredictions; i++) { // and class labels
+        for (let i = 0; i < maxPredictions; i++) { 
           labelContainerEl.current?.appendChild(document.createElement("div"));
         }
     }
@@ -76,7 +66,6 @@ function App() {
     }
 
     async function predict() {
-      // predict can take in an image, video or canvas html element
       const prediction = await model.predict(webcam.canvas);
       for (let i = 0; i < maxPredictions; i++) {
           const classPrediction =
