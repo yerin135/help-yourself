7f194969b2f8ac193a6468d49eafe5a0bde46556
yerin jeon <rin9682@daum.net>
2022-06-21 21:35:13 +0900

    almost done!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from '@emotion/styled';
 import './App.css';
 import * as tf from '@tensorflow/tfjs';
 import * as tmImage from '@teachablemachine/image';
+import { model } from '@tensorflow/tfjs';
+import { setSourceMapRange, setSyntheticTrailingComments } from 'typescript';
+import { cursorTo } from 'readline';
 
 const Container = styled.div`
   font-size: 1.2rem;
@@ -10,9 +13,10 @@ const Container = styled.div`
 
 const Title = styled.div`
   align-items: center;
-  font-family: 'Space Mono';
-  font-size: 3rem;
-  margin: 2rem;
+  font-family: 'Gravity Handwritten Regular';
+  color: #b197fc;
+  font-size: 8rem;
+  margin-top: 2rem;
 `;
 
 const Webcam = styled.div`
@@ -21,11 +25,14 @@ const Webcam = styled.div`
 
 const Label = styled.div`
   line-height: 2rem;
+  font-weight: 300;
 `;
+
+
 
 function App() {
   const initialized = useRef(false);
-
+  const [scores, setScores] = useState<{className: string, probability: number}[]>();
 
   const webcamContainerEl = useRef<HTMLDivElement>(null);
   const labelContainerEl = useRef<HTMLDivElement>(null);
@@ -38,6 +45,9 @@ function App() {
     let model: tmImage.CustomMobileNet;
     let maxPredictions: number;
     let webcam: tmImage.Webcam;
+    // let Item: number;
+    let lastPredictedAt = new Date(0);
+    let lastPrediction: string;
 
     async function init() {
         const modelURL = URL + "model.json";
@@ -52,6 +62,7 @@ function App() {
         await webcam.setup(); 
         await webcam.play();
         window.requestAnimationFrame(loop);
+        // window.requestAnimationFrame(loop2);
 
         webcamContainerEl.current?.appendChild(webcam.canvas);
         for (let i = 0; i < maxPredictions; i++) { 
@@ -65,16 +76,59 @@ function App() {
       window.requestAnimationFrame(loop);
     }
 
+    // async function loop2() {
+    //   setTimeout(() => window.requestAnimationFrame(loop2), 3000);
+    //   const prediction = await model.predict(webcam.canvas);
+    //   console.log(prediction[0].probability);
+    //   setScores(prediction);
+
+    //   const  mmm=prediction?.reduce((prev, curr) => prev.probability >= curr.probability ? prev : curr);
+    //   if (!mmm) return
+    //   const synth = window.speechSynthesis;
+    //   synth.speak(new SpeechSynthesisUtterance(mmm.className));
+    // }
+    
     async function predict() {
       const prediction = await model.predict(webcam.canvas);
-      for (let i = 0; i < maxPredictions; i++) {
-          const classPrediction =
-              prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-          labelContainerEl.current!.children[i].innerHTML = classPrediction;
+      // console.log(prediction[0].probability);
+      setScores(prediction);
+
+      const  mmm=prediction?.reduce((prev, curr) => prev.probability >= curr.probability ? prev : curr);
+      if (!mmm) return
+
+      console.log(mmm, lastPrediction)
+
+      if (lastPrediction === mmm.className && (new Date().getTime() - lastPredictedAt.getTime()) > 1000) {
+        const synth = window.speechSynthesis;
+        synth.speak(new SpeechSynthesisUtterance(mmm.className));
+        lastPredictedAt = new Date(9999999999999)
       }
+
+      if (lastPrediction !== mmm.className) {
+        lastPrediction = mmm.className;
+        lastPredictedAt = new Date();
+      }
+      
+
+      // max(prediction.probability)
+      // if(prediction[0].probability > 0.5 ){
+      //   console.log('choco');
+        
+      // }
+      // for (let i = 0; i < maxPredictions; i++) {
+      //         prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+      //     labelContainerEl.current!.children[i].innerHTML = classPrediction;
+      // }
   }
+    
   init();
+  // loop2();
+  // loop();
   }, []);
+
+const maxClass = scores?.reduce((prev, curr) => 
+  prev.probability >= curr.probability ? prev : curr
+)
 
   return (
     <div className="App">
@@ -82,6 +136,7 @@ function App() {
         <Title>
           Help Yourself
         </Title>
+        
         <Container>
           <Webcam>
             <div ref={webcamContainerEl}>
@@ -89,8 +144,15 @@ function App() {
             </div>
           </Webcam>
           <Label>
-          <div ref={labelContainerEl}>
-
+          <div>
+              {/* {scores?.map(({className, probability}) => ( */}
+              <div>
+                {/* <div>{className}</div>
+                <div>{probability.toFixed(2)}</div> */}
+                <div>{maxClass?.className}</div>
+              </div>
+            {/* )) */}
+            {/* } */}
           </div>
           </Label>
         </Container>
